1a40ae6c05a7e4f1b4330e185a6a7f48528597ad
Fancisco <franciscocabralfilho@gmail.com>
2015-11-12 17:58:41 -0300

    Remoção de quantidade em estoque quando fizer um pedido. Adição de quantidade em estoque quando cancelar um pedido não finalizado.

--- app/models/pedido.rb
+++ app/models/pedido.rb
@@ -6,4 +6,8 @@ class Pedido < ActiveRecord::Base
   validates_presence_of :idProduto, :on => :create, :message => "O campo produtos deve ser informado"
   validates_presence_of :idcomanda, :on => :create, :message => "O campo comanda deve ser informado"
   validates_presence_of :dataPedido, :on => :create, :message => "O campo data do pedido deve ser informado"
+
+  def is_finalizado
+    return self.dataFinalizacao != nil
+  end
 end
--- app/models/produto.rb
+++ app/models/produto.rb
@@ -7,4 +7,20 @@ class Produto < ActiveRecord::Base
   validates_presence_of :nome, :on => :create, :message => "O campo nome deve ser informado"
   validates_presence_of :valor, :on => :create, :message => "O campo valor deve ser informado"
   validates_presence_of :descricao, :on => :create, :message => "O campo descricao deve ser informado"
+
+  def quantidade
+    componentes = self.componentes_produtos
+
+    menor_quantidade = -1;
+    componentes.each do |componente|
+      if(menor_quantidade == -1)
+        menor_quantidade = componente.item.quantidade / componente.quantidade
+      else
+        quantidade_temporaria = componente.item.quantidade / componente.quantidade
+        menor_quantidade = menor_quantidade > quantidade_temporaria ? quantidade_temporaria : menor_quantidade;
+      end
+    end
+    return menor_quantidade > -1 ? menor_quantidade : 0
+  end
+
 end
