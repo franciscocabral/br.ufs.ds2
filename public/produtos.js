ac6c20bf1df8c8b62f02e5eb7205371db3a523fa
Renato Silveira <renatosnj@gmail.com>
2015-11-28 22:03:13 -0200

    Ajustes no Javascript

--- public/produtos.js
+++ public/produtos.js
@@ -2,8 +2,7 @@ var pdtItensSelecionados = [];
 var tabelaDeItens = document.getElementById("lista-de-itens");
 var tabelaDeItensSelecionados = document.getElementById("tabela-selecionados");
 
-function pdtListarItensSelecionados() {
-    
+function pdtChecarItensSelecionados() {    
     var tamanho = tabelaDeItens.rows.length;
     for (var i = 1; i < tamanho; i++) {
         var linha = tabelaDeItens.rows[i];
@@ -15,6 +14,7 @@ function pdtListarItensSelecionados() {
         if (input.checked) {
             if (linha.childNodes[7].childNodes[0].value === "0" || linha.childNodes[7].childNodes[0].value === "") {
                 alert("Voce precisa definir a quantidade do item: " + linha.childNodes[3].textContent);
+
                 break;
             } else {
                 var itemAtual = new pdtItem(
@@ -40,26 +40,35 @@ function pdtListarItensSelecionados() {
                         pdtItensSelecionados.push(itemAtual);
                     }
                 }
+                document.getElementById("alert_container").style.display = 'none';
+                $('#nova-comanda').modal('hide');
+                pdtListarItensSelecionados();
             }
         }
     }
+}
 
+var pdtListarItensSelecionados = function(){
     while (tabelaDeItensSelecionados.rows.length > 1) {
         document.getElementById('tabela-selecionados').deleteRow(1);
     }
     for (var item of pdtItensSelecionados) {
         tabelaDeItensSelecionados.innerHTML += item.getLinhaDaTabela();
     }
-
-    $('#nova-comanda').modal('hide');
-
 }
-
-function pdtRemoverItem(indice) {
-    tabelaDeItensSelecionados.deleteRow(indice);
-    for (var row of tabelaDeItens.rows) {
-        alert(row[0]);
+var pdtRemoverItem = function(indice) {
+    var itemId = tabelaDeItensSelecionados.rows[indice].id;
+    for (var i = 1; i < tabelaDeItens.rows.lengh; i++) {
+        var linha = tabelaDeItens.rows[i];
+        var coluna = linha.childNodes[1];
+        var div = coluna.childNodes[1];
+        var span = div.childNodes[0];
+        var input = span.childNodes[0];
+        if(input.id == itemId){
+            pdtItemAlterado(input);
+        }
     }
+    tabelaDeItensSelecionados.deleteRow(indice);
 }
 
 var pdtItem = function(id, nome, marca,quantidade) {
@@ -70,7 +79,7 @@ var pdtItem = function(id, nome, marca,quantidade) {
 
 
     this.getLinhaDaTabela = function() {
-        var linha = "<tr>";
+        var linha = "<tr id="+ id +">";
         linha += "<td>" + this.nome + "</td>";
         linha += "<td>" + this.marca + "</td>";
         linha += "<td>" + this.quantidade + "</td>";
@@ -83,7 +92,7 @@ var pdtItem = function(id, nome, marca,quantidade) {
         this.quantidade = qtd;
     }
 }
-var produto_item_alterado = function(item_checkbox){
+var pdtItemAlterado = function(item_checkbox){
     if(!item_checkbox.checked){
          var linha = item_checkbox.parentNode.parentNode.parentNode.parentNode;
          linha.childNodes[7].childNodes[0].value  = "";
@@ -91,8 +100,11 @@ var produto_item_alterado = function(item_checkbox){
             if (item.id === item_checkbox.value){
                 index = pdtItensSelecionados.indexOf(item);
                 pdtItensSelecionados.splice(item, 1);
+                pdtListarItensSelecionados();
                 break;
             }
         }
     }
 }
+
+//atualizar lista de item ao excluir
